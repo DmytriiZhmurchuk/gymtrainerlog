3bbd4e8e37da08d28f3c1f6276269536acc2abb2
Dmytrii Zhmurchuk <dmytriizhmurchuk@MacBook-Air-Dmytrii.local>
2021-10-26 00:23:23 +0300

    handle save and add toast

--- components/exercises/AddEditExercise.js
+++ components/exercises/AddEditExercise.js
@@ -1,29 +1,50 @@
-import React from 'react';
-import {View, Text, TextInput, SafeAreaView, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 import {Navigation} from 'react-native-navigation';
 import {CancelButton, SaveButton} from '../widgets/buttons';
 import Input from '../widgets/Input';
+import {RootSiblingParent} from 'react-native-root-siblings';
+import {showToast} from '../utils';
 
 const AddEditExercise = props => {
+  const [exercise, setExercise] = useState('');
+
+  const handleExcerciseChange = value => {
+    setExercise(value);
+  };
+
   const handleCancel = () => {
     Navigation.dismissModal(props.componentId);
   };
-  const handleSave = () => {};
+
+  const handleSave = () => {
+    if (!exercise.length) {
+      showToast('Exercise name  cannot be empty');
+      return;
+    }
+
+    showToast('Saved Successfully');
+  };
   return (
-    <View style={styles.container}>
-      <View style={styles.exercise}>
-        <Text style={styles.inputLabel}>Exercise title:</Text>
-        <Input placeholder="Enter exercise name" />
-      </View>
-      <View style={styles.row}>
-        <View style={styles.btnCancel}>
-          <CancelButton onPress={handleCancel} />
+    <RootSiblingParent>
+      <View style={styles.container}>
+        <View style={styles.exercise}>
+          <Text style={styles.inputLabel}>Exercise title:</Text>
+          <Input
+            placeholder="Enter exercise name"
+            onChangeText={handleExcerciseChange}
+          />
         </View>
-        <View style={styles.btnSave}>
-          <SaveButton onPress={handleSave} />
+        <View style={styles.row}>
+          <View style={styles.btnCancel}>
+            <CancelButton onPress={handleCancel} />
+          </View>
+          <View style={styles.btnSave}>
+            <SaveButton onPress={handleSave} />
+          </View>
         </View>
       </View>
-    </View>
+    </RootSiblingParent>
   );
 };
 
