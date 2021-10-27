c9f5f5e5f753048b829ed34f5410c4589987b777
Dmytrii Zhmurchuk <dmytriizhmurchuk@MacBook-Air-Dmytrii.local>
2021-10-27 23:57:59 +0300

    style client list

--- components/clients/ClientsList.js
+++ components/clients/ClientsList.js
@@ -1,23 +1,54 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, TextInput, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+} from 'react-native';
 import {Navigation} from 'react-native-navigation';
 import {AddButton} from '../widgets/buttons';
 import {getAllClients, openDBConnection} from '../db';
 import {showToast} from '../utils';
 import {RootSiblingParent} from 'react-native-root-siblings';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {ListItem, Avatar, SearchBar} from 'react-native-elements';
 
 const ClientsList = () => {
-  const [clients, setClients] = useState([]);
+  const [listState, setListState] = useState({
+    data: [],
+    limit: 10,
+    startIndex: 0,
+  });
+  const [search, setSearch] = useState();
+
+  const updateSearch = value => {
+    setSearch(value);
+  };
 
   const fetchClients = async () => {
     try {
       const db = await openDBConnection();
-      const results = await getAllClients(db);
+      const results = await getAllClients(
+        db,
+        listState.limit,
+        listState.startIndex,
+      );
+      if (!results[0].rows.length) {
+        return;
+      }
+
       var temp = [];
       for (let i = 0; i < results[0].rows.length; ++i) {
         temp.push(results[0].rows.item(i));
       }
-      setClients(temp);
+
+      setListState({
+        ...listState,
+        startIndex: listState.startIndex + listState.limit,
+        data: listState.data.concat(temp),
+      });
     } catch (error) {
       showToast('DB error');
     }
@@ -49,34 +80,88 @@ const ClientsList = () => {
     });
   };
 
+  const openClient = () => {
+    alert('open');
+  };
+
+  const renderItem = ({item}) => {
+    return (
+      <ListItem key={item.id} bottomDivider onPress={openClient}>
+        <Avatar
+          rounded
+          size="medium"
+          icon={{name: 'user', type: 'font-awesome'}}
+          onPress={() => console.log('Works!')}
+          activeOpacity={0.7}
+          overlayContainerStyle={{backgroundColor: '#eeeeee'}}
+        />
+        <ListItem.Content>
+          <ListItem.Title style={{fontSize: 20}}>
+            {item.firstName}
+          </ListItem.Title>
+          <ListItem.Subtitle style={{fontSize: 20}}>
+            {item.lastName}
+          </ListItem.Subtitle>
+        </ListItem.Content>
+        <ListItem.Chevron size={30} />
+      </ListItem>
+    );
+  };
+
+  const renderEmptyList = () => {
+    return (
+      <View
+        style={{
+          marginTop: '50%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={{fontSize: 16}}>
+          There are no items. Please press add new client
+        </Text>
+      </View>
+    );
+  };
+
   useEffect(() => {
     fetchClients();
   }, []);
 
   return (
-    <RootSiblingParent>
-      <SafeAreaView>
+    <SafeAreaProvider>
+      <RootSiblingParent>
         <View style={styles.container}>
-          <View style={styles.row}>
-            <AddButton title={'Add New Client'} onPress={showAddNewClient} />
+          <View style={{marginTop: 10}}>
+            <SearchBar
+              platform="ios"
+              placeholder="Type Here..."
+              onChangeText={updateSearch}
+              value={search}
+            />
           </View>
-          <View>
-            <TextInput placeholder="Search" />
-          </View>
-          <View>
-            {clients.map(client => {
-              return <Text>{`${client.firstName}-${client.lastName}`}</Text>;
-            })}
+          <View style={{flex: 1}}>
+            <FlatList
+              data={listState.data}
+              renderItem={renderItem}
+              keyExtractor={item => item.id}
+              onEndReached={fetchClients}
+              extraData={listState}
+              ListEmptyComponent={renderEmptyList}
+            />
+            <View
+              style={{paddingHorizontal: 10, marginBottom: 50, marginTop: 10}}>
+              <AddButton title={'Add new client'} onPress={showAddNewClient} />
+            </View>
           </View>
         </View>
-      </SafeAreaView>
-    </RootSiblingParent>
+      </RootSiblingParent>
+    </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 60,
+    flex: 1,
   },
   row: {
     flexDirection: 'row',
