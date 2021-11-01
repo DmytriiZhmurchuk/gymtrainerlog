cc45f7c27aad785b4d50ea29801f488cd2d32a24
DmytriiZhmurchuk <d.zhmurchuk@gmail.com>
2021-11-01 20:39:29 +0200

    renaming (#18)

--- ios/GymTrainerLogsTests/GymTrainerLogsTests.m
+++ ios/GymTrainerLogsTests/GymTrainerLogsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface demoappTests : XCTestCase
+@interface GymTrainerLogsTests : XCTestCase
 
 @end
 
-@implementation demoappTests
+@implementation GymTrainerLogsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
